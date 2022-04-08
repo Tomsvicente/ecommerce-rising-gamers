0ff049321fb5eded5ce7357f5262fef8daa0ec99
Tomas <tomsebvicente@gmail.com>
2022-04-07 23:05:58 -0300

    AgregadoProductos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar';
-import ItemListContainer from './components/ItemListContainer';
-import ItemCount from './components/ItemCount';
+import ItemListContainer from './components/Items/ItemListContainer';
+
 
 export default function App() {
   return <>
     <div className="App">
       <NavBar />
-
-      <ItemListContainer teclados={"Red Dragon"} monitor={"LG"} mouse={"Logitech Daedalus Prime"}/>
+      <ItemListContainer/>
 
     </div>
   </>;
